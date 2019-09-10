daf3ee76cd91938712f956df62dadd7164dfbb78
Yehonathan Jacob <yehonathanjacob@gmail.com>
2019-09-10 18:42:59 +0300

    starting the semninar

---
+++ CPP/Yehonathan_Jacob_seminar/CilentSide/cppFiles/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "main.h"
+#include "inputs.h"
+#include "connections.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!\n"; // prints Hello World
+	cout << "Hello Yhonathan!\n"; // prints Hello World
+	return 0;
+}
