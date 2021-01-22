dd33dbbbfec02c3515731c6506f23b8ea9296405
Yehonathan <yehonathanjacob@gmail.com>
2021-01-22 14:07:11 +0200

    removing yadcapital porject

--- PythonLab/PersonalProjects/yadcapital_aws/app/default_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_PATH = os.getcwd()
-HOST = '0.0.0.0'
-PORT = '8000'
-DATA_JSON_PATH = os.path.join(BASE_PATH, 'datafiles', 'production.json')
-BASE_URL = 'https://yadcapital.com'
-DEBUG = False
--- PythonLab/PersonalProjects/yadcapital_aws/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import json
-from app.init_app import config
-
-def get_data():
-    path_to_data = config.DATA_JSON_PATH
-    with open(path_to_data, 'r') as file:
-        data = json.load(file)
-    return data
