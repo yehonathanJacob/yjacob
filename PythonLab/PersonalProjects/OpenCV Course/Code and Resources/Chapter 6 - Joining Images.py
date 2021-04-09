997cf62e83e1b2e8178de55a65e03d3005b397bf
Yehonathan Jacob <yehonathanj@aidoc.com>
2021-04-09 16:31:34 +0300

    adding OpenCV Course

---
+++ PythonLab/PersonalProjects/OpenCV Course/Code and Resources/Chapter 6 - Joining Images.py
@@ -0,0 +1,60 @@
+# Joining Images
+import cv2
+import numpy as np
+
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+kernel = np.ones((5, 5), np.uint8)
+
+img = cv2.imread('Resources/lena.png')
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
+imgCanny = cv2.Canny(img, 150, 200)
+imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)
+imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
+
+imgStack = stackImages(0.5, ([img, imgGray, imgBlur], [imgCanny, imgDialation, imgEroded]))
+
+# imgHor = np.hstack((img,img))
+# imgVer = np.vstack((img,img))
+#
+# cv2.imshow("Horizontal",imgHor)
+# cv2.imshow("Vertical",imgVer)
+cv2.imshow("ImageStack", imgStack)
+cv2.setWindowProperty('ImageStack', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
