1b8c15f7dd77fcc29534d78326a187a1442e6e46
Yehonathan <yehonathanj@aidoc.com>
2021-07-08 13:46:50 +0300

    change mycollege croller to be dynamic

---
+++ PythonLab/PersonalProjects/crolling/mycollegestats/config.py
@@ -0,0 +1,11 @@
+CHROM_DRIVER = '/home/yjacob/Documents/drivers/chromedriver'
+MAIN_URL = 'https://www.mycollegestats.com/output'
+USER_MAIL = 'yehonathanjacob@gmail.com'
+USER_PASSWORD = 'test123'
+
+HEADER_COLUMNS = ['Year Applied', 'School Name', 'Visited', 'Sex', 'Year of Birth', 'Race', 'Country of Citizenship',
+                  'Country of Residence', 'State of Residence', '1st Language', '2nd Language', 'High School',
+                  'U/W GPA', 'GPA Max', 'SAT Math', 'SAT Reading', 'SAT Total', 'ACT', 'Essay', 'SAT II Subject',
+                  'SAT II Score', 'SAT II Subject', 'SAT II Score', 'College Decision', 'Student Decision']
+
+URL_TO_COLLEGE = 'https://www.mycollegestats.com/applicationresult/{college_name}'
